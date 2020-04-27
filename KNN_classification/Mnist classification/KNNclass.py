08af42c1a4b986e091c4a8aa65a7aa8bf512f7d1
Min Joon <48172859+artiiicy@users.noreply.github.com>
2020-04-27 11:37:46 +0900

    Update KNNclass.py

--- KNN_classification/Mnist classification/KNNclass.py
+++ KNN_classification/Mnist classification/KNNclass.py
@@ -7,9 +7,9 @@ class knn:
         self.y = y
 
     def show(self):
-        print(self.k)
-        print(self.x)
-        print(self.y)
+        print("k :", self.k)
+        print("x :", self.x)
+        print("y :", self.y)
 
     # Calculate the distance btw test data and train data sets
     def cal_distance(self, input_x, input_y):
@@ -20,7 +20,7 @@ class knn:
             distance = 0
             j = 0
             while j < input_x.shape[0]:
-                distance += (input_x[j] - self.x[i][j])**2
+                distance += (int(input_x[j]) - int(self.x[i][j]))**2
                 j += 1
             distance = distance ** (1/2)
             dis_pair = [[distance, i]]
@@ -28,7 +28,7 @@ class knn:
             i += 1
         return distances
 
-    # Obtain k classes
+    # Obtain k classes : 정렬하여 상위 k개를 list slicing을 이용하여 추출
     def obtain_KNN(self, distances):
         distances.sort()
         idx_list = []
@@ -41,21 +41,16 @@ class knn:
             j += 1
         return idx_list, dis_list
 
-    # Obtain class with Majority Vote method
+    # Obtain class with Majority Vote method : class_list의 값 중 어떠한 값이 가장 많은지를 조건문을 통해 파악하여 반환하고 카운팅은 +1로 한다. class_num = class의 개수
     def obtain_mv(self, class_list, class_num):
         count = np.zeros(class_num)
 
         for i in class_list:
-            if i == 0:
-                count[0] += 1
-            elif i == 1:
-                count[1] += 1
-            else:
-                count[2] += 1
+            count[i] += 1
 
         return np.argmax(count)
 
-    # Obtain class with Weighted Majority Vote method
+    # Obtain class with Weighted Majority Vote method : class_list의 값 중 어떠한 값이 가장 많은지를 조건문을 통해 파악하여 반환하고 카운팅은 1/distance로 한다. class_num = class의 개수
     def obtain_wmv(self, class_list, dis_list, class_num):
         count = np.zeros(class_num)
 
@@ -70,3 +65,9 @@ class knn:
             j += 1
 
         return np.argmax(count)
+
+    def get_score(self, output, answer):
+        if output == answer:
+            return 1
+        else:
+            return 0
