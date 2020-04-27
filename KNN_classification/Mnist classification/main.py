936eec6c594e3e261f2664b5853c18d97516fc4b
Min Joon <48172859+artiiicy@users.noreply.github.com>
2020-04-27 22:04:09 +0900

    Update main.py

--- KNN_classification/Mnist classification/main.py
+++ KNN_classification/Mnist classification/main.py
@@ -4,6 +4,7 @@ sys.path.append(os.pardir)
 import numpy as np
 from dataset.mnist import load_mnist
 from PIL import Image
+import random
 
 import KNNclass
 
@@ -18,8 +19,20 @@ def img_show(img):
 # 컴퓨터 과부화를 줄이기 위하여 Train Data의 일부만을 사용.
 x_train = x_train[:30000]
 y_train = y_train[:30000]
-x_test = x_test[:100]
-y_test = y_test[:100]
+
+# Test를 위한 Data 임의 추출
+rand_array = np.zeros(10000, dtype=bool)
+ran_num = random.randint(0, len(x_test) - 1)
+
+for i in range(100):
+    while rand_array[ran_num] == 1:
+        ran_num = random.randint(0, len(x_test) - 1)
+    rand_array[ran_num] = 1
+
+x_test_rand = x_test[rand_array]
+y_test_rand = y_test[rand_array]
+
+# Label name 및 K 값 list에 저장
 label_name = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 k_list = [3]
 
@@ -29,12 +42,12 @@ for k_value in k_list:
 
     i = 0
     score = 0
-    while i < len(x_test):
-        distances = new_knn.cal_distance(x_test[i], y_test[i])
+    while i < len(x_test_rand):
+        distances = new_knn.cal_distance(x_test_rand[i], y_test_rand[i])
         class_list, dis_list = new_knn.obtain_KNN(distances)
-        result = new_knn.obtain_mv(class_list, len(label_name))
-        print("Test Data Index:", i, "    Computed class:", label_name[result], "    True class:", label_name[y_test[i]])
+        result = new_knn.obtain_wmv(class_list, dis_list, len(label_name))
+        print("Test Data Index:", i, "    Computed class:", label_name[result], "    True class:", label_name[y_test_rand[i]])
         i += 1
-        score += new_knn.get_score(label_name[result], label_name[y_test[i-1]])
+        score += new_knn.get_score(label_name[result], label_name[y_test_rand[i-1]])
 
     print('accuracy :', '%.2f' % (score/i))
