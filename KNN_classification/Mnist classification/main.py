0cffbb7209ff0b80ef574bd460f265f75a9be9c8
Min Joon <48172859+artiiicy@users.noreply.github.com>
2020-04-28 23:22:44 +0900

    Update main.py

--- KNN_classification/Mnist classification/main.py
+++ KNN_classification/Mnist classification/main.py
@@ -1,12 +1,10 @@
 import sys, os
 sys.path.append(os.pardir)
-
 import numpy as np
 from dataset.mnist import load_mnist
 from PIL import Image
 import random
 import time
-
 import KNNclass
 
 def img_show(img):
