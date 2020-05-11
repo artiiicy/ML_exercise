a4907bbeee1b1b8483d9b484737b996d60f53f3e
Min Joon <48172859+artiiicy@users.noreply.github.com>
2020-05-11 17:58:32 +0900

    Update and rename functions.py to function.py

--- function.py
+++ function.py
@@ -35,7 +35,7 @@ def softmax(x):
         y = np.exp(x) / np.sum(np.exp(x), axis=0)
         return y.T 
 
-    x = x - np.max(x) # 오버플로 대책
+    x = x - np.max(x) # 오버플로우 대책
     return np.exp(x) / np.sum(np.exp(x))
 
 
