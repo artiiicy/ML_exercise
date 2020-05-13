e072b8c9ade5f1bc543a9a3765875d868f14d09e
Min Joon <48172859+artiiicy@users.noreply.github.com>
2020-05-14 02:42:44 +0900

    Update main.py

--- Logistic_Regression/Singleclass/Iris classification/main.py
+++ Logistic_Regression/Singleclass/Iris classification/main.py
@@ -5,6 +5,11 @@ import time
 import matplotlib.pyplot as plt
 import logisticRegression
 
+def one_hot_encoding(input):
+    num = np.unique(input, axis=0)  # num = y array 중 unique한 값들로만 이루어진 array
+    num = num.shape[0]  # num = y array 중 unique한 값들의 개수
+    return np.eye(num)[input], num  # np.eye = 단위행렬을 만드는 함수. 즉, y의 unique한 개수만큼의 row를 가지는 단위행렬을 만들고 y에 해당하는 row를 추출한다.
+
 # Load Iris Data set
 iris = load_iris()
 
@@ -18,30 +23,39 @@ X_train, X_test, y_train, y_test\
     = train_test_split(X, y, test_size=1/15, shuffle=True, random_state=int(time.time()))  # sklearn의 데이터분할 내장함수 사용.
                                                                                            # test_size : 전체 데이터의 몇 %를 test data로 사용할지 지정
                                                                                            # shuffle : 셔플 여부 설정, random_state : 셔플을 위한 시드 값 지정
+y_train, num = one_hot_encoding(y_train)
+y_test, num = one_hot_encoding(y_test)
+# num = np.unique(y_train, axis=0)  # num = y array 중 unique한 값들로만 이루어진 array
+# num = num.shape[0]  # num = y array 중 unique한 값들의 개수
+# y_train = np.eye(num)[y_train]  # np.eye = 단위행렬을 만드는 함수. 즉, y의 unique한 개수만큼의 row를 가지는 단위행렬을 만들고 y에 해당하는 row를 추출한다.
+# num = np.unique(y_train, axis=0)
+# num = num.shape[0]
+# y_train = np.eye(num)[y_train]
 
-num = np.unique(y_train, axis=0)  # num = y array 중 unique한 값들로만 이루어진 array
-num = num.shape[0]  # num = y array 중 unique한 값들의 개수
-y_train = np.eye(num)[y_train]  # np.eye = 단위행렬을 만드는 함수. 즉, y의 unique한 개수만큼의 row를 가지는 단위행렬을 만들고 y에 해당하는 row를 추출한다.
+LRmodel, cost_arr = [], []
+i = 0
+while i < num:
+    print("\n***", i, "th Logistic Regression model ***")
+    LRmodel.append(logisticRegression.logisticRegression(X_train, y_train[:, i], 'single'))
+    cost_arr.append(LRmodel[i].learn(learning_rate=0.0001, epoch=100))
+    i += 1
 
-LRmodel = logisticRegression.logisticRegression(0.001, X_train, y_train)
-
-start = time.time()
-iteration_array = []
-cost_array = []
-
-for i in range(1,101):
-    iteration_array.append(i)
-    cost_array.append(LRmodel.cost())
-    print("epoch:", i, "  cost:", cost_array[i - 1])
-    LRmodel.learn()
-
-prediction_Time = time.time() - start
-accuracy = LRmodel.predict(X_test, y_test)
-print("accuracy:", accuracy, "  Prediction time:", prediction_Time)
-
-plt.plot(iteration_array, cost_array)
-plt.title('Loss Graph')
-plt.xlabel('number of iteration')
-plt.ylabel('cost')
-plt.legend(['0','1','2','3','4','5','6','7','8','9'],loc='upper right')
+i = 1
+for graph in cost_arr:
+    plt.subplot(num, 2, i)
+    plt.plot(graph[0], graph[1])
+    plt.title('%s-th Model''s Loss Graph' %str(i-1))
+    plt.xlabel('number of iteration')
+    plt.ylabel('cost')
+    plt.legend(str(i-1), loc='upper right')
+    i += 1
+plt.tight_layout()
 plt.show()
+
+# train Data set
+i = 0
+while i < num:
+    print("\n***", i, "th Logistic Regression model ***")
+    accuracy = LRmodel[i].predict(X_test, y_test[:, i])
+    print("accuracy:", accuracy, "  score:", int(accuracy*y_test.shape[0]), "/", y_test.shape[0])
+    i += 1
