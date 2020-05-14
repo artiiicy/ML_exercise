8666ad7bfa1cbf80f6ac935daaa0d65ec4eb0bb7
Min Joon <48172859+artiiicy@users.noreply.github.com>
2020-05-14 11:03:32 +0900

    Update main.py

--- Logistic_Regression/Singleclass/Mnist classification/main.py
+++ Logistic_Regression/Singleclass/Mnist classification/main.py
@@ -2,28 +2,35 @@ import sys, os
 sys.path.append(os.pardir)
 import numpy as np
 from dataset.mnist import load_mnist
-import random
+import matplotlib.pyplot as plt
 import logisticRegression
 
-def generate_rand_data(origin_size, rand_size):
-    rand_array = np.zeros(origin_size, dtype=bool)
-    ran_num = random.randint(0, origin_size - 1)
-
-    for i in range(rand_size):
-        while rand_array[ran_num] == 1:
-            ran_num = random.randint(0, origin_size - 1)
-        rand_array[ran_num] = 1
-    return rand_array
-
 # Load Mnist Data set
-(x_train, y_train), (x_test, y_test) = load_mnist(flatten=True, normalize=True)#, one_hot_label=True)
+(x_train, y_train), (x_test, y_test) = load_mnist(flatten=True, normalize=True, one_hot_label=True)
 num = np.unique(y_train, axis=0)  # num = y array 중 unique한 값들로만 이루어진 array
 num = num.shape[0]  # num = y array 중 unique한 값들의 개수
-y_train = np.eye(num)[y_train]  # np.eye = 단위행렬을 만드는 함수. 즉, y의 unique한 개수만큼의 row를 가지는 단위행렬을 만들고 y에 해당하는 row를 추출한다.
 
-LRmodel = logisticRegression.logisticRegression(x_train, y_train)
+LRmodel, cost_arr = [], []
+i = 0
+while i < num:
+    print("\n***", i, "th Logistic Regression model ***")
+    LRmodel.append(logisticRegression.logisticRegression(x_train, y_train[:, i], 'single'))
+    cost_arr.append(LRmodel[i].learn(learning_rate=0.1, epoch=2))
+    i += 1
+
+for graph in cost_arr:
+    plt.plot(graph[0], graph[1])
+plt.title('Binary-Class Model''s Loss Graph')
+plt.xlabel('number of iteration')
+plt.ylabel('cost')
+plt.legend(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], loc='upper right')
+plt.tight_layout()
+plt.show()
 
 # train Data set
-LRmodel.learn(learning_rate=0.001, epoch=10)
-accuracy = LRmodel.predict(x_test, y_test)
-print("accuracy:", accuracy, "  score:", int(accuracy*y_test.shape[0]), "/", y_test.shape[0])
+i = 0
+while i < num:
+    print("\n***", i, "th Logistic Regression model ***")
+    accuracy = LRmodel[i].predict(x_test, y_test[:, i])
+    print("accuracy:", accuracy, "  score:", int(accuracy*y_test.shape[0]), "/", y_test.shape[0])
+    i += 1
