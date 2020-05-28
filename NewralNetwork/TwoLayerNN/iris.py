0abadcb3949f421f4a54f4311c08030534a98b9a
Min Joon <48172859+artiiicy@users.noreply.github.com>
2020-05-29 01:49:01 +0900

    Update iris.py

--- NewralNetwork/TwoLayerNN/iris.py
+++ NewralNetwork/TwoLayerNN/iris.py
@@ -1,15 +1,14 @@
-import time
 import NeuralNetworkclass as NNclass
 
 # hyper-parameter values
-batch_size = 30
-epoch_num = 1000
-learning_rate = 0.0009
-hidden_size = 5
+batch_size = 60
+epoch_num = 10000
+learning_rate = 0.003
+hidden_size = 15
 
 input_size = 4
 output_size = 3
 
 # Two layer neural network 생성 후 학습
 twoLayerNetwork = NNclass.TwoLayerNetwork(input_size=input_size, hidden_size=hidden_size, output_size=output_size)
-twoLayerNetwork.learn(lr=learning_rate, epoch=epoch_num, batch_size=batch_size)
+twoLayerNetwork.learn(lr=learning_rate, epoch=epoch_num, batch_size=batch_size, verbose=True)
